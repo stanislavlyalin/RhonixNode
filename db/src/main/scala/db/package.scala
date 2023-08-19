cb71b04061ef0978d766516ccd72c2c2559f3ffe
stanislavlyalin <lyalinstas@gmail.com>
2023-08-19 15:26:36 +0300

    Added service functions for creating session and executing queries to the DB

---
+++ db/src/main/scala/db/package.scala
@@ -0,0 +1,16 @@
+import org.squeryl.adapters.PostgreSqlAdapter
+import sdk.CustomTypeMode
+import sdk.db.DbSession
+
+import java.sql.Connection
+
+package object db {
+  def session[F[_]](connection: Connection): DbSession[F] = new DbSession[F] {
+    override def using[A](a: => A): A = CustomTypeMode.using(session)(a)
+
+    private lazy val session = {
+      val _ = Class.forName("org.postgresql.Driver")
+      org.squeryl.Session.create(connection, new PostgreSqlAdapter)
+    }
+  }
+}
