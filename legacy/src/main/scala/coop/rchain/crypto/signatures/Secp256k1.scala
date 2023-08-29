281d7d1b8330f6852b6f0c07d372cf6af0918dc1
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-08-29 21:29:22 +0200

    Copy crypto package

---
+++ legacy/src/main/scala/coop/rchain/crypto/signatures/Secp256k1.scala
@@ -0,0 +1,150 @@
+package coop.rchain.crypto.signatures
+
+import java.io.FileReader
+import java.nio.file.Path
+import java.security.KeyPairGenerator
+import java.security.interfaces.ECPrivateKey
+import java.security.spec.ECGenParameterSpec
+import cats.effect.Sync
+import cats.syntax.applicative._
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+import cats.syntax.applicativeError._
+import coop.rchain.crypto.util.SecureRandomUtil
+import coop.rchain.crypto.{PrivateKey, PublicKey}
+import org.bitcoin._
+import com.google.common.base.Strings
+import coop.rchain.shared.Base16
+import org.bouncycastle.asn1.DLSequence
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.openssl.bc.BcPEMDecryptorProvider
+import org.bouncycastle.openssl.{PEMEncryptedKeyPair, PEMParser}
+
+// TODO: refactor Signature API to handle exceptions from `NativeSecp256k1` library
+
+object Secp256k1 extends SignaturesAlg {
+
+  private val provider        = new BouncyCastleProvider()
+  val name                    = "secp256k1"
+  override val sigLength: Int = 32
+
+  /**
+    * Verifies the given secp256k1 signature in native code.
+    *
+    * @return (private key, public key) pair
+    *
+    */
+  @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
+  def newKeyPair: (PrivateKey, PublicKey) = {
+    val kpg = KeyPairGenerator.getInstance("ECDSA", provider)
+    kpg.initialize(new ECGenParameterSpec(name), SecureRandomUtil.secureRandomNonBlocking)
+    val kp  = kpg.generateKeyPair
+
+    val padded =
+      Strings.padStart(kp.getPrivate.asInstanceOf[ECPrivateKey].getS.toString(16), 64, '0')
+    val sec    = Base16.unsafeDecode(padded)
+    val pub    = Secp256k1.toPublic(sec)
+
+    (PrivateKey(sec), PublicKey(pub))
+  }
+
+  def parsePemFile[F[_]: Sync](path: Path, password: String): F[PrivateKey] = {
+    import scala.jdk.CollectionConverters._
+
+    def handleWith[A](f: => A, message: String): F[A] =
+      Sync[F].delay(f).recoverWith { case t => Sync[F].raiseError(new Exception(message, t)) }
+
+    val parser = new PEMParser(new FileReader(path.toFile))
+    for {
+      encryptedKeyPair <- handleWith(
+                            parser.readObject().asInstanceOf[PEMEncryptedKeyPair],
+                            "PEM file is not encrypted",
+                          )
+      decryptor         = new BcPEMDecryptorProvider(password.toCharArray)
+      keyPair          <- handleWith(
+                            encryptedKeyPair.decryptKeyPair(decryptor),
+                            "Could not decrypt PEM file",
+                          )
+      dlSequence       <- handleWith(
+                            keyPair.getPrivateKeyInfo.parsePrivateKey.asInstanceOf[DLSequence],
+                            "Could not parse private key from PEM file",
+                          )
+      privateKeyOpt     = dlSequence.iterator().asScala.collectFirst { case octet: org.bouncycastle.asn1.DEROctetString =>
+                            PrivateKey(octet.getOctets)
+                          }
+      privateKey       <- privateKeyOpt match {
+                            case Some(privateKey) => privateKey.pure[F]
+                            case None             =>
+                              Sync[F].raiseError[PrivateKey](
+                                new Exception("PEM file does not contain private key"),
+                              )
+                          }
+    } yield privateKey
+  }
+
+  // TODO: refactor to make use of strongly typed keys
+  /**
+    * Verifies the given secp256k1 signature in native code.
+    *
+    * Input values
+    * @param data The data which was signed, must be exactly 32 bytes
+    * @param signature The signature
+    * @param pub The public key which did the signing
+    *
+    * Return value
+    * boolean value of verification
+    *
+    */
+  def verify(
+    data: Array[Byte],
+    signature: Array[Byte],
+    pub: Array[Byte],
+  ): Boolean =
+    // WARNING: this code throws Assertion exception if input is not correct length
+    NativeSecp256k1.verify(data, signature, pub)
+
+  /**
+    * libsecp256k1 Create an ECDSA signature.
+    *
+    * Input values
+    * @param data Message hash, 32 bytes
+    * @param sec Secret key, 32 bytes
+    *
+    * Return value
+    * byte array of signature
+    *
+    */
+  def sign(
+    data: Array[Byte],
+    sec: Array[Byte],
+  ): Array[Byte] =
+    // WARNING: this code throws Assertion exception if input is not correct length
+    NativeSecp256k1.sign(data, sec)
+
+  /**
+    * libsecp256k1 Seckey Verify - returns true if valid, false if invalid
+    *
+    * Input value
+    * @param seckey ECDSA Secret key, 32 bytes
+    *
+    * Return value
+    * Boolean of secret key verification
+    */
+  def secKeyVerify(seckey: Array[Byte]): Boolean =
+    // WARNING: this code throws Assertion exception if input is not correct length
+    NativeSecp256k1.secKeyVerify(seckey)
+
+  /**
+    * libsecp256k1 Compute Pubkey - computes public key from secret key
+    *
+    * @param seckey ECDSA Secret key, 32 bytes
+    *
+    * Return values
+    * @param pubkey ECDSA Public key, 33 or 65 bytes
+    */
+  def toPublic(seckey: Array[Byte]): Array[Byte] =
+    // WARNING: this code throws Assertion exception if input is not correct length
+    NativeSecp256k1.computePubkey(seckey)
+
+  override def toPublic(sec: PrivateKey): PublicKey = PublicKey(toPublic(sec.bytes))
+}
---
+++ legacy/src/main/scala/coop/rchain/crypto/util/CertificateHelper.scala
@@ -0,0 +1,219 @@
+package coop.rchain.crypto.util
+
+import coop.rchain.crypto.hash.Keccak256
+import coop.rchain.sdk.syntax.all._
+import coop.rchain.shared.Base16
+import org.bouncycastle.asn1._
+import org.bouncycastle.util.BigIntegers
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File, FileInputStream}
+import java.math.BigInteger
+import java.security._
+import java.security.cert._
+import java.security.interfaces.{ECPrivateKey, ECPublicKey}
+import java.security.spec._
+import java.util.Base64
+import scala.io.Source
+import scala.util.Using.Releasable
+import scala.util.{Failure, Try, Using}
+
+object CertificateHelper {
+
+  val EllipticCurveName = "secp256r1"
+
+  lazy val EllipticCurveParameterSpec: ParameterSpec = {
+    val ap = AlgorithmParameters.getInstance("EC")
+    ap.init(new ECGenParameterSpec(EllipticCurveName))
+    ParameterSpec(ap.getParameterSpec(classOf[ECParameterSpec]))
+  }
+
+  def isExpectedEllipticCurve(publicKey: PublicKey): Boolean =
+    publicKey match {
+      case p: ECPublicKey =>
+        ParameterSpec(p.getParams) == EllipticCurveParameterSpec
+      case _              => false
+    }
+
+  @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+  def publicAddress(publicKey: PublicKey): Option[Array[Byte]] =
+    publicKey match {
+      case p: ECPublicKey if isExpectedEllipticCurve(publicKey) =>
+        val publicKey = Array.ofDim[Byte](64)
+        val x         = p.getW.getAffineX.toByteArray.takeRight(32)
+        val y         = p.getW.getAffineY.toByteArray.takeRight(32)
+        x.copyToArray(publicKey, 32 - x.length)
+        y.copyToArray(publicKey, 64 - y.length)
+        Some(publicAddress(publicKey))
+      case _                                                    => None
+    }
+
+  def publicAddress(input: Array[Byte]): Array[Byte] =
+    Keccak256.hash(input).drop(12)
+
+  def from(certFilePath: String): Try[X509Certificate] =
+    fromFile(new File(certFilePath))
+
+  @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
+  def fromFile(certFile: File): Try[X509Certificate] =
+    Using(new FileInputStream(certFile)) { file =>
+      val cf = CertificateFactory.getInstance("X.509")
+      cf.generateCertificate(file).asInstanceOf[X509Certificate]
+    }
+
+  @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
+  def readKeyPair(keyFile: File): KeyPair = {
+    val str      = Using.resource(Source.fromFile(keyFile, "utf8")) {
+      _.getLines().filter(!_.contains("KEY")).mkString
+    }
+    val spec     = new PKCS8EncodedKeySpec(Base64.getDecoder.decode(str))
+    val kf       = KeyFactory.getInstance("EC")
+    val sk       = kf.generatePrivate(spec).asInstanceOf[ECPrivateKey]
+    val ecSpec   = org.bouncycastle.jce.ECNamedCurveTable.getParameterSpec(EllipticCurveName)
+    val Q        = ecSpec.getG.multiply(sk.getS).normalize()
+    val pubPoint = new ECPoint(Q.getAffineXCoord.toBigInteger, Q.getAffineYCoord.toBigInteger)
+    val pubSpec  = new ECPublicKeySpec(pubPoint, sk.getParams)
+    val pk       = kf.generatePublic(pubSpec)
+    new KeyPair(pk, sk)
+  }
+
+  def generateKeyPair(useNonBlockingRandom: Boolean): KeyPair = {
+    val secureRandom =
+      if (useNonBlockingRandom) SecureRandomUtil.secureRandomNonBlocking
+      else new SecureRandom()
+    val kpg          = KeyPairGenerator.getInstance("EC")
+    kpg.initialize(new ECGenParameterSpec(EllipticCurveName), secureRandom)
+    kpg.generateKeyPair
+  }
+
+  @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
+  def generate(keyPair: KeyPair): X509Certificate = {
+    import sun.security.x509._
+
+    val privateKey  = keyPair.getPrivate
+    val publicKey   = keyPair.getPublic
+    val address     = publicAddress(publicKey).map(Base16.encode).getOrElse("local")
+    val algorythm   = "SHA256withECDSA"
+    val algorithmId = new AlgorithmId(AlgorithmId.sha256WithECDSA_oid)
+
+    val info     = new X509CertInfo
+    val from     = new java.util.Date()
+    val to       = new java.util.Date(from.getTime + 365 * 86400000L)
+    val interval = new CertificateValidity(from, to)
+    val serial   = new BigInteger(64, new SecureRandom())
+    val owner    = new X500Name(s"CN=$address")
+
+    info.set(X509CertInfo.VALIDITY, interval)
+    info.set(X509CertInfo.SERIAL_NUMBER, new CertificateSerialNumber(serial))
+    info.set(X509CertInfo.SUBJECT, owner)
+    info.set(X509CertInfo.ISSUER, owner)
+    info.set(X509CertInfo.KEY, new CertificateX509Key(publicKey))
+    info.set(X509CertInfo.VERSION, new CertificateVersion(CertificateVersion.V3))
+    info.set(X509CertInfo.ALGORITHM_ID, new CertificateAlgorithmId(algorithmId))
+
+    // Sign the cert to identify the algorithm that's used.
+    val cert = new X509CertImpl(info)
+    cert.sign(privateKey, algorythm)
+
+    // Update the algorithm, and resign.
+    val algorithmId2 = cert.get(X509CertImpl.SIG_ALG).asInstanceOf[AlgorithmId]
+    info.set(CertificateAlgorithmId.NAME + "." + CertificateAlgorithmId.ALGORITHM, algorithmId2)
+    val cert2        = new X509CertImpl(info)
+    cert2.sign(privateKey, algorythm)
+    cert2
+  }
+
+  def encodeSignatureRStoDER(signatureRS: Array[Byte]): Try[Array[Byte]] = {
+    def toASN1Int(bytes: Array[Byte]) = new ASN1Integer(BigIntegers.fromUnsignedByteArray(bytes))
+
+    def convert: Try[Array[Byte]] = {
+      val (r, s) = signatureRS.take(64).splitAt(32)
+
+      implicit val relSeq: Releasable[DERSequenceGenerator] = _.close()
+
+      Using
+        .Manager { use =>
+          val bos = use(new ByteArrayOutputStream(72))
+          val seq = use(new DERSequenceGenerator(bos))
+          seq.addObject(toASN1Int(r))
+          seq.addObject(toASN1Int(s))
+          bos
+        }
+        .map(_.toByteArray)
+    }
+
+    if (signatureRS.isEmpty)
+      Failure(new IllegalArgumentException("Input array must not be empty"))
+    else
+      convert.mapFailure(
+        new IllegalArgumentException("Input array is not valid signature", _),
+      )
+  }
+
+  def decodeSignatureDERtoRS(signatureDER: Array[Byte]): Try[Array[Byte]] = {
+    def toBytes(x: ASN1Encodable) = {
+      val asn1        = x.toASN1Primitive.asInstanceOf[ASN1Integer]
+      // IMPORTANT: Specifying length will left pad zeroes
+      val bytesLength = 32
+      BigIntegers.asUnsignedByteArray(bytesLength, asn1.getValue)
+    }
+
+    def convert: Try[Array[Byte]] =
+      Using
+        .Manager { use =>
+          val bis = use(new ByteArrayInputStream(signatureDER))
+          val asn = use(new ASN1InputStream(bis))
+          asn.readObject.asInstanceOf[ASN1Sequence]
+        }
+        .map { asnSeq =>
+          val Array(r, s, _*) = asnSeq.toArray
+          toBytes(r) ++ toBytes(s)
+        }
+    if (signatureDER.isEmpty)
+      Failure(new IllegalArgumentException("Input array must not be empty"))
+    else
+      convert.mapFailure(
+        new IllegalArgumentException("Input array is not valid DER message format", _),
+      )
+  }
+}
+
+final case class ParameterSpec(
+  curve: EllipticCurve,
+  generator: ECPoint,
+  order: BigInt,
+  cofactor: Int,
+)
+
+case object ParameterSpec {
+  def apply(ecParamSpec: ECParameterSpec): ParameterSpec =
+    ParameterSpec(
+      ecParamSpec.getCurve,
+      ecParamSpec.getGenerator,
+      ecParamSpec.getOrder,
+      ecParamSpec.getCofactor,
+    )
+}
+
+object CertificatePrinter {
+
+  import scala.annotation.tailrec
+
+  def print(certificate: X509Certificate): String = {
+    val str = Base64.getEncoder.encodeToString(certificate.getEncoded)
+    split(str).mkString("-----BEGIN CERTIFICATE-----\n", "\n", "\n-----END CERTIFICATE-----")
+  }
+
+  def printPrivateKey(privateKey: PrivateKey): String = {
+    val str = Base64.getEncoder.encodeToString(privateKey.getEncoded)
+    split(str).mkString("-----BEGIN PRIVATE KEY-----\n", "\n", "\n-----END PRIVATE KEY-----")
+  }
+
+  @tailrec
+  private def split(s: String, acc: List[String] = Nil): List[String] =
+    if (s.length == 0) acc.reverse
+    else {
+      val (a, b) = s.splitAt(64)
+      split(b, a :: acc)
+    }
+
+}
