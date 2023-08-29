d72de2e08ae512917703afa52dabf9d6c907ec47
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-08-29 21:43:37 +0200

    Add scalalogging dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,28 +17,29 @@ object Dependencies {
   val grpcNetty = "io.grpc" % "grpc-netty" % "1.53.0"
 
   // LEGACY dependencies of imported projects
-  val protobuf          = "com.google.protobuf"   % "protobuf-java"   % "3.22.2"
-  val jaxb              = "javax.xml.bind"        % "jaxb-api"        % "2.3.1"
-  val scalapbRuntimeLib = "com.thesamet.scalapb" %% "scalapb-runtime" % "0.11.13"
-  val scalapbCompiler   = "com.thesamet.scalapb" %% "compilerplugin"  % "0.11.13"
-  val magnolia          = "com.propensive"       %% "magnolia"        % "0.17.0"
-  val bouncyProvCastle  = "org.bouncycastle"      % "bcprov-jdk15on"  % "1.70"
-  val bouncyPkixCastle  = "org.bouncycastle"      % "bcpkix-jdk15on"  % "1.70"
-  val guava             = "com.google.guava"      % "guava"           % "31.1-jre"
-  val secp256k1Java     = "com.github.rchain"     % "secp256k1-java"  % "0.1"
-  val scodecCore        = "org.scodec"           %% "scodec-core"     % "1.11.10"
-  val scodecCats        = "org.scodec"           %% "scodec-cats"     % "1.2.0"
-  val scodecBits        = "org.scodec"           %% "scodec-bits"     % "1.1.37"
-  val shapeless         = "com.chuusai"          %% "shapeless"       % "2.3.10"
-  val lz4               = "org.lz4"               % "lz4-java"        % "1.8.0"
-  val lmdbjava          = "org.lmdbjava"          % "lmdbjava"        % "0.8.3"
-  val enumeratum        = "com.beachape"         %% "enumeratum"      % "1.7.2"
-  val xalan             = "xalan"                 % "xalan"           % "2.7.3"
-  val catsMtl           = "org.typelevel"        %% "cats-mtl-core"   % "0.7.1"
-  val catsMtlLaws       = "org.typelevel"        %% "cats-mtl-laws"   % "1.3.0"
-  val kalium            = "com.github.rchain"     % "kalium"          % "0.8.1"
-  val lightningj        = "org.lightningj"        % "lightningj"      % "0.5.2-Beta"
-  val scalacheck        = "org.scalacheck"       %% "scalacheck"      % "1.14.1"
+  val protobuf          = "com.google.protobuf"         % "protobuf-java"   % "3.22.2"
+  val jaxb              = "javax.xml.bind"              % "jaxb-api"        % "2.3.1"
+  val scalapbRuntimeLib = "com.thesamet.scalapb"       %% "scalapb-runtime" % "0.11.13"
+  val scalapbCompiler   = "com.thesamet.scalapb"       %% "compilerplugin"  % "0.11.13"
+  val magnolia          = "com.propensive"             %% "magnolia"        % "0.17.0"
+  val bouncyProvCastle  = "org.bouncycastle"            % "bcprov-jdk15on"  % "1.70"
+  val bouncyPkixCastle  = "org.bouncycastle"            % "bcpkix-jdk15on"  % "1.70"
+  val guava             = "com.google.guava"            % "guava"           % "31.1-jre"
+  val secp256k1Java     = "com.github.rchain"           % "secp256k1-java"  % "0.1"
+  val scodecCore        = "org.scodec"                 %% "scodec-core"     % "1.11.10"
+  val scodecCats        = "org.scodec"                 %% "scodec-cats"     % "1.2.0"
+  val scodecBits        = "org.scodec"                 %% "scodec-bits"     % "1.1.37"
+  val shapeless         = "com.chuusai"                %% "shapeless"       % "2.3.10"
+  val lz4               = "org.lz4"                     % "lz4-java"        % "1.8.0"
+  val lmdbjava          = "org.lmdbjava"                % "lmdbjava"        % "0.8.3"
+  val enumeratum        = "com.beachape"               %% "enumeratum"      % "1.7.2"
+  val xalan             = "xalan"                       % "xalan"           % "2.7.3"
+  val catsMtl           = "org.typelevel"              %% "cats-mtl-core"   % "0.7.1"
+  val catsMtlLaws       = "org.typelevel"              %% "cats-mtl-laws"   % "1.3.0"
+  val kalium            = "com.github.rchain"           % "kalium"          % "0.8.1"
+  val lightningj        = "org.lightningj"              % "lightningj"      % "0.5.2-Beta"
+  val scalacheck        = "org.scalacheck"             %% "scalacheck"      % "1.14.1"
+  val scalaLogging      = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.4"
   val legacyLibs        = Seq(
     magnolia,
     guava,
@@ -60,6 +61,7 @@ object Dependencies {
     lightningj,
     shapeless,
     scalacheck,
+    scalaLogging,
   )
 
   // Testing frameworks
