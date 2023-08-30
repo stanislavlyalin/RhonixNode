bde64492a0aa9f5fbbc5728d2f18d22625c12646
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-08-30 08:59:49 +0200

    EqualsM to macro project

--- legacy/src/main/scala/coop/rchain/models/EqualMImplicits.scala
+++ legacy/src/main/scala/coop/rchain/models/EqualMImplicits.scala
@@ -1,59 +1,20 @@
 package coop.rchain.models
 
 import cats.effect.Sync
-import cats.instances.stream._
-import cats.syntax.all._
+import cats.instances.stream.*
+import cats.syntax.all.*
 import com.google.protobuf.ByteString
-import coop.rchain.catscontrib.Catscontrib._
+import coop.rchain.catscontrib.Catscontrib.*
 import coop.rchain.crypto.hash.Blake2b512Random
 import coop.rchain.crypto.signatures.Signed
 import cats.Eval
+import coop.rchain.models.EqualM.*
+import magnolia.Magnolia.gen
 
 import scala.Function.tupled
 import scala.collection.immutable.BitSet
 
-/**
-  * A typeclass for computing Scala's default `equals` for case classes in a stacksafe manner.
-  *
-  * An instance of this class must be provided for every case where `equals` is overridden.
-  * The generative tests should catch that, provided the offending case class is used transitively.
-  *
-  * @tparam A
-  */
-trait EqualM[A] {
-
-  def equal[F[_]: Sync](self: A, other: A): F[Boolean]
-
-  def equals[F[_]: Sync](a: A, b: Any): F[Boolean] =
-    if (a.getClass.isInstance(b)) {
-      equal[F](a, a.getClass.cast(b))
-    } else false.pure[F]
-
-}
-
-object EqualM extends EqualMDerivation {
-
-  def apply[A](implicit ev: EqualM[A]): EqualM[A] = ev
-
-  def by[A, B: EqualM](f: A => B): EqualM[A] = new EqualM[A] {
-
-    override def equal[F[_]: Sync](self: A, other: A): F[Boolean] =
-      EqualM[B].equal(f(self), f(other))
-
-  }
-
-  /**
-    * EqualM instance delegating to Any#equa(). Hence the type bound.
-    * This MUST NOT be used for types having a recursive structure
-    * nor for types whose EqualCode() delegates to others.
-    * Otherwise the stacksafety provided by the EqualM typeclass is broken.
-    *
-    * @tparam A
-    * @return
-    */
-  def opaqueEqual[A]: EqualM[A] = new EqualM[A] {
-    override def equal[F[_]: Sync](self: A, other: A): F[Boolean] = Sync[F].pure(self == other)
-  }
+object EqualMImplicits {
 
   implicit val IntEqual: EqualM[Int]                           = opaqueEqual
   implicit val BigIntEqual: EqualM[BigInt]                     = opaqueEqual
@@ -150,36 +111,4 @@ object EqualM extends EqualMDerivation {
 
   implicit val PCostHash              = gen[PCost]
   implicit val TaggedContinuationHash = gen[TaggedContinuation]
-}
-
-trait EqualMDerivation {
-  import magnolia._
-
-  type Typeclass[T] = EqualM[T]
-
-  def combine[T](ctx: CaseClass[EqualM, T]): EqualM[T] = new EqualM[T] {
-
-    def equal[F[_]: Sync](self: T, other: T): F[Boolean] = Sync[F].defer {
-      ctx.parameters.to(LazyList).forallM { p =>
-        p.typeclass.equal(p.dereference(self), p.dereference(other))
-      }
-    }
-
-  }
-
-  def dispatch[T](ctx: SealedTrait[EqualM, T]): EqualM[T] = new EqualM[T] {
-
-    def equal[F[_]: Sync](self: T, other: T): F[Boolean] = Sync[F].defer {
-      ctx.dispatch(self) { sub =>
-        if (sub.cast.isDefinedAt(other)) {
-          sub.typeclass.equal(sub.cast(self), sub.cast(other))
-        } else {
-          Sync[F].pure(false)
-        }
-      }
-    }
-
-  }
-
-  implicit def gen[T]: EqualM[T] = macro Magnolia.gen[T]
 }
