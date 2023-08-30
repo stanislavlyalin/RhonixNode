2b393dbe902f3fe65c87535553bee3aaed82b9bf
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-08-30 09:14:20 +0200

    Do the same with HashM

--- legacy/src/main/scala/coop/rchain/models/HashMImplicits.scala
+++ legacy/src/main/scala/coop/rchain/models/HashMImplicits.scala
@@ -2,47 +2,18 @@ package coop.rchain.models
 
 import cats.Contravariant
 import cats.effect.Sync
-import cats.syntax.all._
+import cats.syntax.all.*
 import com.google.protobuf.ByteString
 import coop.rchain.crypto.hash.Blake2b512Random
 import coop.rchain.crypto.signatures.Signed
 import coop.rchain.models.Expr.ExprInstance.{GBigInt, GInt}
+import coop.rchain.models.HashM.*
+import coop.rchain.models.HashMDerivation.*
 
 import java.util.Objects
 import scala.collection.immutable.BitSet
-import scala.util.hashing.MurmurHash3
 
-/**
-  * A typeclass for computing Scala's default `hashCode` for case classes in a stacksafe manner.
-  *
-  * An instance of this class must be provided for every case where `hashCode` is overridden.
-  * The generative tests should catch that, provided the offending case class is used transitively.
-  *
-  * @tparam A
-  */
-trait HashM[A] {
-  def hash[F[_]: Sync](value: A): F[Int]
-}
-
-object HashM extends HashMDerivation {
-
-  def apply[A](implicit ev: HashM[A]): HashM[A] = ev
-
-  /**
-    * HashM instance delegating to .##.
-    *
-    * This MUST NOT be used for types having a recursive structure
-    * nor for types whose hashCode() delegates to others.
-    * Otherwise the stacksafety provided by the HashM typeclass is broken.
-    *
-    * @tparam A
-    * @return
-    */
-  def opaqueHash[A]: HashM[A] = new HashM[A] {
-
-    override def hash[F[_]: Sync](value: A): F[Int] = Sync[F].pure(value.##)
-
-  }
+object HashMImplicits {
 
   implicit val HashMContravariant = new Contravariant[HashM] {
     override def contramap[A, B](fa: HashM[A])(f: B => A): HashM[B] = new HashM[B] {
@@ -176,56 +147,4 @@ object HashM extends HashMDerivation {
 //  implicit val ContinuationAtNameResponseV2Hash = gen[v1.ContinuationAtNameResponse]
 //  implicit val FindDeployResponseV2Hash         = gen[v1.FindDeployResponse]
 //  implicit val LastFinalizedBlockResponseV2Hash = gen[v1.LastFinalizedBlockResponse]
-}
-
-trait HashMDerivation {
-  import magnolia._
-
-  type Typeclass[T] = HashM[T]
-
-  def combine[T](ctx: CaseClass[HashM, T]): HashM[T] = new HashM[T] {
-
-    def hash[F[_]: Sync](value: T): F[Int] = Sync[F].defer {
-      val hashFs = ctx.parameters.map { p =>
-        p.typeclass.hash(p.dereference(value))
-      }
-      for {
-        componentHashes <- hashFs.toList.sequence
-      } yield productHash(ctx.typeName.short, componentHashes)
-    }
-
-  }
-
-  // copied and adapted from scala.util.hashing.MurmurHash3,
-  // which is used in Scala's case class hash code implementation
-  @SuppressWarnings(Array("org.wartremover.warts.Var"))
-  private def productHash(prefix: String, elements: Seq[Int]): Int = {
-    val arr = elements.size
-    // Case objects have the hashCode inlined directly into the
-    // synthetic hashCode method, but this method should still give
-    // a correct result if passed a case object.
-    if (arr == 0) {
-      prefix.hashCode
-    } else {
-      var h = MurmurHash3.productSeed
-      var i = 0
-      while (i < arr) {
-        h = MurmurHash3.mix(h, elements(i))
-        i += 1
-      }
-      MurmurHash3.finalizeHash(h, arr)
-    }
-  }
-
-  def dispatch[T](ctx: SealedTrait[HashM, T]): HashM[T] = new HashM[T] {
-
-    def hash[F[_]: Sync](value: T): F[Int] = Sync[F].defer {
-      ctx.dispatch(value) { sub =>
-        sub.typeclass.hash(sub.cast(value))
-      }
-    }
-
-  }
-
-  implicit def gen[T]: HashM[T] = macro Magnolia.gen[T]
 }
---
+++ macros/src/main/scala/coop/rchain/models/HashM.scala
@@ -0,0 +1,91 @@
+package coop.rchain.models
+
+import cats.effect.Sync
+import cats.syntax.all.*
+
+import scala.util.hashing.MurmurHash3
+
+/**
+ * A typeclass for computing Scala's default `hashCode` for case classes in a stacksafe manner.
+ *
+ * An instance of this class must be provided for every case where `hashCode` is overridden.
+ * The generative tests should catch that, provided the offending case class is used transitively.
+ *
+ * @tparam A
+ */
+trait HashM[A] {
+  def hash[F[_]: Sync](value: A): F[Int]
+}
+
+object HashM {
+
+  def apply[A](implicit ev: HashM[A]): HashM[A] = ev
+
+  /**
+   * HashM instance delegating to .##.
+   *
+   * This MUST NOT be used for types having a recursive structure
+   * nor for types whose hashCode() delegates to others.
+   * Otherwise the stacksafety provided by the HashM typeclass is broken.
+   *
+   * @tparam A
+   * @return
+   */
+  def opaqueHash[A]: HashM[A] = new HashM[A] {
+
+    override def hash[F[_]: Sync](value: A): F[Int] = Sync[F].pure(value.##)
+
+  }
+}
+
+object HashMDerivation {
+  import magnolia.*
+
+  type Typeclass[T] = HashM[T]
+
+  def combine[T](ctx: CaseClass[HashM, T]): HashM[T] = new HashM[T] {
+
+    def hash[F[_]: Sync](value: T): F[Int] = Sync[F].defer {
+      val hashFs = ctx.parameters.map { p =>
+        p.typeclass.hash(p.dereference(value))
+      }
+      for {
+        componentHashes <- hashFs.toList.sequence
+      } yield productHash(ctx.typeName.short, componentHashes)
+    }
+
+  }
+
+  // copied and adapted from scala.util.hashing.MurmurHash3,
+  // which is used in Scala's case class hash code implementation
+  @SuppressWarnings(Array("org.wartremover.warts.Var"))
+  private def productHash(prefix: String, elements: Seq[Int]): Int = {
+    val arr = elements.size
+    // Case objects have the hashCode inlined directly into the
+    // synthetic hashCode method, but this method should still give
+    // a correct result if passed a case object.
+    if (arr == 0) {
+      prefix.hashCode
+    } else {
+      var h = MurmurHash3.productSeed
+      var i = 0
+      while (i < arr) {
+        h = MurmurHash3.mix(h, elements(i))
+        i += 1
+      }
+      MurmurHash3.finalizeHash(h, arr)
+    }
+  }
+
+  def dispatch[T](ctx: SealedTrait[HashM, T]): HashM[T] = new HashM[T] {
+
+    def hash[F[_]: Sync](value: T): F[Int] = Sync[F].defer {
+      ctx.dispatch(value) { sub =>
+        sub.typeclass.hash(sub.cast(value))
+      }
+    }
+
+  }
+
+  implicit def gen[T]: HashM[T] = macro Magnolia.gen[T]
+}
