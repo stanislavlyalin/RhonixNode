efe534c92b08dd153ed48b6fcea3cab6df979f82
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-09-12 09:39:45 +0200

    Fix wart error

--- db/src/main/scala/squeryl/api/BlockDeploysDbApiImpl.scala
+++ db/src/main/scala/squeryl/api/BlockDeploysDbApiImpl.scala
@@ -13,7 +13,7 @@ import squeryl.tables.CustomTypeMode.*
 
 class BlockDeploysDbApiImpl[F[_]: Sync: SqlConn] extends BlockDeploysDbApi[F] {
   override def insert(blockDeploys: BlockDeploys): F[Unit] =
-    withSession(blockDeploysTable.insert(BlockDeploysTable.toDb(blockDeploys))).map(BlockDeploysTable.fromDb)
+    withSession { val _ = blockDeploysTable.insert(BlockDeploysTable.toDb(blockDeploys)); () }
 
   override def getByBlock(blockId: Long): F[Seq[BlockDeploys]] = withSession(
     blockDeploysTable.where(_.blockId === blockId).map(BlockDeploysTable.fromDb).toSeq,
