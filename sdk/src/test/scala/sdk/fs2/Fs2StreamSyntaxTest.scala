5b326f16394f7450da50761dc67eb47a72f2f184
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-09-23 13:51:14 +0200

    Ignore test for throughput syntax

--- sdk/src/test/scala/sdk/fs2/Fs2StreamSyntaxTest.scala
+++ sdk/src/test/scala/sdk/fs2/Fs2StreamSyntaxTest.scala
@@ -10,7 +10,10 @@ import sdk.syntax.all.*
 import scala.concurrent.duration.DurationInt
 
 class Fs2StreamSyntaxTest extends AnyFlatSpec with Matchers {
-  "throughput" should "output correct value" in {
+  // This test cannot run on CI, since fs2.Stream has no guarantee that value is immediately pulled
+  // when data is available. So output might depend on the environment where test is run.
+  // E.g. if host is busy, test might fail.
+  "throughput" should "output correct value" ignore {
     val stream = Stream.repeatEval[IO, Unit](IO.unit).metered(90.millis).take(100)
     val out    = stream.throughput(1.second).compile.toList.unsafeRunSync()
     out shouldBe List(11, 11, 11, 11, 11, 11, 11, 11, 11)
