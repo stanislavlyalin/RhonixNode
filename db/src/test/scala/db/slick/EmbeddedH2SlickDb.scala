5b2bf0fae5861df02c6c4c1eb397dc22577a0cea
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-10-02 17:36:47 +0300

    Make H2 inmem database persist content until close is called

--- db/src/test/scala/db/slick/EmbeddedH2SlickDb.scala
+++ db/src/test/scala/db/slick/EmbeddedH2SlickDb.scala
@@ -3,14 +3,17 @@ package db.slick
 import cats.effect.kernel.Async
 import cats.effect.{Resource, Sync}
 import slick.SlickDb
-import slick.jdbc.{H2Profile, JdbcBackend}
+import slick.jdbc.H2Profile
 import slick.jdbc.JdbcBackend.Database
 import slick.migration.api.H2Dialect
 
-// Slick interface for embedded Postgres. Only for testing.
+// Slick interface for in memory H2. Only for testing.
 object EmbeddedH2SlickDb {
   def apply[F[_]: Async]: Resource[F, SlickDb] = {
-    val dbResource = Resource.make(Sync[F].delay(Database.forURL("jdbc:h2:mem:")))(db => Sync[F].delay(db.close()))
+    // db should be named (:test), and keepAliveConnection set to true,
+    // otherwise something weird happens with the db life cycle and db is cleared before closed.
+    val open       = Sync[F].delay(Database.forURL("jdbc:h2:mem:test", keepAliveConnection = true))
+    val dbResource = Resource.make(open)(db => Sync[F].delay(db.close()))
 
     SlickDb(dbResource, H2Profile, new H2Dialect)
   }
