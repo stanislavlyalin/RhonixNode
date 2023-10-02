7f367a1934d4093f2c25d8d90a66bba69d380972
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-10-02 19:29:11 +0300

    Remove obsolete comment

--- db/src/test/scala/db/slick/SlickSpec.scala
+++ db/src/test/scala/db/slick/SlickSpec.scala
@@ -12,9 +12,6 @@ import sdk.db.*
 import slick.SlickDb
 import slick.api.*
 
-// TODO re-enable these tests when it is possible to execute them on CI.
-// At the moment Github Actions does not allow non-root users and embedded postgres requires non-root.
-// https://github.com/fergusstrange/embedded-postgres/issues/95
 class SlickSpec extends AsyncFlatSpec with Matchers with ScalaCheckPropertyChecks {
 
   "Validator insert function call" should "add the correct entry to the Validator table" in {
