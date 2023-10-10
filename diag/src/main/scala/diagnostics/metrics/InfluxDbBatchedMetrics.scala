ee4c1c68eca2940ca5e2b4cd07fc42143fefb3b0
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-10-10 15:50:24 +0400

    Remove import

--- diag/src/main/scala/diagnostics/metrics/InfluxDbBatchedMetrics.scala
+++ diag/src/main/scala/diagnostics/metrics/InfluxDbBatchedMetrics.scala
@@ -9,6 +9,7 @@ import com.influxdb.client.{InfluxDBClient, InfluxDBClientFactory, InfluxDBClien
 import okhttp3.OkHttpClient
 import sdk.diag.Metrics
 import sdk.diag.Metrics.{Field, Tag}
+import scala.jdk.CollectionConverters.*
 
 import scala.concurrent.duration.Duration
 import scala.util.Try
@@ -55,38 +56,33 @@ object InfluxDbBatchedMetrics {
    * @param config configuration object
    * @param sourceTag tag of a source. Useful to show many instantiations of the same metric on the same chart
    */
-  def apply[F[_]: Async](config: Config, sourceTag: String): Resource[F, Metrics[F]] = {
-    import config.*
+  def apply[F[_]: Async](config: Config, sourceTag: String): Resource[F, Metrics[F]] = for {
+    client <- Resource.make(mkInfluxClient(config))(c => Sync[F].delay(c.close()))
+    writer <- Resource.make(mkAsyncWriter(client))(w => Sync[F].delay { w.flush(); w.close() })
+    // accumulator
+    acc    <- Resource.eval(Ref.of(List.empty[Point]))
+    times  <- Resource.eval(Ref.of(Map.empty[String, Long]))
+    // fiber doing infinitely repeating reports
+    f       = acc.getAndSet(List()).map(points => writer.writePoints(points.asJava))
+    _      <- reportingFiberResource(f, config.batchInterval)
+  } yield new Metrics[F] {
+    override def measurement(name: String, fields: List[Metrics.Field], tags: List[Metrics.Tag]): F[Unit] =
+      Clock[F].realTimeInstant.flatMap { now =>
+        lazy val p = Point
+          .measurement(name)
+          .addTag(SOURCE_TAG_KEY, sourceTag)
+          .time(now, WritePrecision.MS)
+          .addTags(tags.map { case Tag(k, v) => k -> v }.toMap.asJava)
+          .addFields(fields.map { case Field(k, v) => k -> v }.toMap.asJava)
 
-    import scala.jdk.CollectionConverters.*
-    for {
-      client <- Resource.make(mkInfluxClient(config))(c => Sync[F].delay(c.close()))
-      writer <- Resource.make(mkAsyncWriter(client))(w => Sync[F].delay { w.flush(); w.close() })
-      // accumulator
-      acc    <- Resource.eval(Ref.of(List.empty[Point]))
-      times  <- Resource.eval(Ref.of(Map.empty[String, Long]))
-      // fiber doing infinitely repeating reports
-      f       = acc.getAndSet(List()).map(points => writer.writePoints(points.asJava))
-      _      <- reportingFiberResource(f, batchInterval)
-    } yield new Metrics[F] {
-      override def measurement(name: String, fields: List[Metrics.Field], tags: List[Metrics.Tag]): F[Unit] =
-        Clock[F].realTimeInstant.flatMap { now =>
-          lazy val p = Point
-            .measurement(name)
-            .addTag(SOURCE_TAG_KEY, sourceTag)
-            .time(now, WritePrecision.MS)
-            .addTags(tags.map { case Tag(k, v) => k -> v }.toMap.asJava)
-            .addFields(fields.map { case Field(k, v) => k -> v }.toMap.asJava)
-
-          times
-            .modify { s =>
-              val nowMillis    = now.toEpochMilli
-              val shouldUpdate = s.get(name).forall(x => (nowMillis - x) >= config.meter.toMillis)
-              val newS         = if (shouldUpdate) s + (name -> nowMillis) else s
-              newS -> shouldUpdate
-            }
-            .flatMap(acc.update(_ :+ p).whenA(_))
-        }
-    }
+        times
+          .modify { s =>
+            val nowMillis    = now.toEpochMilli
+            val shouldUpdate = s.get(name).forall(x => (nowMillis - x) >= config.meter.toMillis)
+            val newS         = if (shouldUpdate) s + (name -> nowMillis) else s
+            newS -> shouldUpdate
+          }
+          .flatMap(acc.update(_ :+ p).whenA(_))
+      }
   }
 }
