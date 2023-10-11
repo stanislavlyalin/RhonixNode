33328ca883f6f1fcc28633c1371fdb132c0030ae
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-10-11 16:57:47 +0400

    Rename Blake2b spec, make it ByteArray32 spec

--- sdk/src/test/scala/sdk/hashing/ByteArray32Spec.scala
+++ sdk/src/test/scala/sdk/hashing/ByteArray32Spec.scala
@@ -5,30 +5,10 @@ import org.scalacheck.{Arbitrary, Gen, Prop}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.scalacheck.Checkers
 import sdk.history.ByteArray32
-import sdk.history.ByteArray32.codec
 import sdk.primitive.ByteArray
 import sdk.syntax.all.*
 
-import java.util
-
-class Blake2bSpec extends AnyFlatSpec with Checkers {
-
-  "The bytes of a Blake2b256 hash" should "be the same as if it was created directly" in {
-    val propCreate: Prop = Prop.forAll { (bytes: Array[Byte]) =>
-      util.Arrays.equals(Blake2b.hash256(bytes), ByteArray32(bytes).bytes.bytes)
-    }
-    check(propCreate)
-  }
-
-  "A Blake2b256 hash" should "be the same when round-tripped with scodec" in {
-    val propRoundTripCodec: Prop = Prop.forAll { (bytes: Array[Byte]) =>
-      val hash    = ByteArray32(bytes)
-      val encoded = codec.encode(hash)
-      val decoded = codec.decode(encoded.getUnsafe)
-      hash.bytes == decoded.getUnsafe.bytes
-    }
-    check(propRoundTripCodec)
-  }
+class ByteArray32Spec extends AnyFlatSpec with Checkers {
 
   "Method fromByteArray()" should "create Blake2b256Hash from 32 byte array" in {
     val byteArrayGen: Gen[Array[Byte]] = Gen.listOfN(ByteArray32.Length, Arbitrary.arbitrary[Byte]).map(_.toArray)
