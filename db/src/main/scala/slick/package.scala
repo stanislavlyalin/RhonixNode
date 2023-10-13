f79a536ebe0749303309bb2d5f456019617b3fd7
stanislavlyalin <slyalin@naumen.ru>
2023-10-13 11:28:08 +0300

    Add TableConfig table, migration and query functions

--- db/src/main/scala/slick/package.scala
+++ db/src/main/scala/slick/package.scala
@@ -1,9 +1,10 @@
 import slick.lifted.TableQuery
-import slick.tables.{TableBonds, TableValidators}
+import slick.tables.{TableBonds, TableConfig, TableValidators}
 
 package object slick {
 
   // all queries
   val bonds      = TableQuery[TableBonds]
   val validators = TableQuery[TableValidators]
+  val config     = TableQuery[TableConfig]
 }
