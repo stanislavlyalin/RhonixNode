4313d279d4a3bf5312afa2400abcdcf24a2151e1
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-10-16 21:46:21 +0400

    Add types for queries

--- db/src/main/scala/slick/SlickQuery.scala
+++ db/src/main/scala/slick/SlickQuery.scala
@@ -1,5 +1,6 @@
 package slick
 
+import slick.dbio.Effect.{Read, Write}
 import slick.jdbc.JdbcProfile
 import slick.sql.SqlAction
 import slick.tables.TableValidators.Validator
@@ -7,16 +8,17 @@ import slick.tables.TableValidators.Validator
 final case class SlickQuery()(implicit val profile: JdbcProfile) {
   import profile.api.*
 
-  def getValidatorById(id: Long) =
+  def getValidatorById(id: Long): SqlAction[Option[Validator], NoStream, Read] =
     qValidators.filter(_.id === id).result.headOption
 
-  def getValidatorByPubKey(publicKey: Array[Byte]) =
+  def getValidatorByPubKey(publicKey: Array[Byte]): SqlAction[Option[Validator], NoStream, Read] =
     qValidators.filter(_.pubKey === publicKey).result.headOption
 
-  def insertValidator(publicKey: Array[Byte]) =
+  def insertValidator(publicKey: Array[Byte]): SqlAction[Long, NoStream, Write] =
     (qValidators.map(r => r.pubKey) returning qValidators.map(_.id)) += publicKey
 
-  def updateValidator(validator: Validator) = qValidators.update(validator)
+  def updateValidator(validator: Validator): SqlAction[Int, NoStream, Write] =
+    qValidators.update(validator)
 
   def storeValue(key: String, value: String): SqlAction[Int, NoStream, Effect.Write] =
     qConfigs.insertOrUpdate((key, value))
