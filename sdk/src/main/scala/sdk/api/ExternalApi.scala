7676abd144d36043f5ec9442f0ed88d6ab3612f0
stanislavlyalin <lyalinstas@gmail.com>
2023-10-19 14:44:06 +0300

    Add external API definitions and stub implementation

--- sdk/src/main/scala/sdk/api/ExternalApi.scala
+++ sdk/src/main/scala/sdk/api/ExternalApi.scala
@@ -3,6 +3,16 @@ package sdk.api
 import sdk.api.data.*
 
 trait ExternalApi[F[_]] {
-  def getBlockById(blockId: Long): F[Option[Block]]
-  def getDeploysByBlockId(blockId: Long): F[Seq[Deploy]]
+  // Block API
+  def getBlockByHash(hash: Array[Byte]): F[Option[Block]]
+
+  // Deploy API
+  def deploy(deploy: Deploy): F[Long]
+  def getDeployStatus(hash: Array[Byte]): F[Int]
+  def getDeploysByBlockHash(hash: Array[Byte]): F[Seq[Deploy]]
+
+  // Diagnostic
+  def status: F[Status]
+  def getLatestMessage: F[Block]
+  def visualizeDag[R](depth: Int, showJustificationLines: Boolean): F[Vector[String]]
 }
---
+++ sdk/src/main/scala/sdk/api/data/Status.scala
@@ -0,0 +1,5 @@
+package sdk.api.data
+
+final case class Status(
+  shardId: String,
+)
