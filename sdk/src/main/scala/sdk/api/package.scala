7f02c7f0cda2d36f7ab9f2ea6616a6297fc221a8
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-11-07 23:04:12 +0400

    Set values for api definition in sdk

--- sdk/src/main/scala/sdk/api/package.scala
+++ sdk/src/main/scala/sdk/api/package.scala
@@ -2,6 +2,8 @@ package sdk
 
 package object api {
   private val Namespace     = List("api")
-  private val Version       = "v1"
+  val Version               = "v1"
   val RootPath: Seq[String] = Namespace :+ Version
+  val Title: String         = "Gorki node API"
+  val DocFileName: String   = "openapi.json"
 }
