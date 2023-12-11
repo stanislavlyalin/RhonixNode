679178bc26b899eb0de30e698a7fbbd7e6ce8127
Tomislav Grospic <grospic@gmail.com>
2023-12-11 13:38:01 +0100

    Add remainder NormalizerRec interface (temp)

--- legacy/src/main/scala/coop/rchain/rholang/normalizer2/NormalizerRec.scala
+++ legacy/src/main/scala/coop/rchain/rholang/normalizer2/NormalizerRec.scala
@@ -1,12 +1,18 @@
 package coop.rchain.rholang.normalizer2
 
 import io.rhonix.rholang.ParN
-import io.rhonix.rholang.ast.rholang.Absyn.{Name, Proc}
+import io.rhonix.rholang.ast.rholang.Absyn.{Name, NameRemainder, Proc, ProcRemainder}
 
 trait NormalizerRec[F[_]] {
   def normalize(proc: Proc): F[ParN]
 
   def normalize(proc: Name): F[ParN]
+
+  // TODO: Remove when reminder will be replaced with more general spread operator.
+
+  def normalize(proc: ProcRemainder): F[ParN]
+
+  def normalize(proc: NameRemainder): F[ParN]
 }
 
 object NormalizerRec {
