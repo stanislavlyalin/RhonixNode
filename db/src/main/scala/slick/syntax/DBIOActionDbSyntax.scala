2f168f9d40d0ffcea119455d6adc0da7b014a1c8
stanislavlyalin <lyalinstas@gmail.com>
2023-12-13 15:51:08 +0300

    Use repeats for transaction if it meets SERIALIZATION_FAILURE error when performing parallel insert on Serializable isolation level

--- db/src/main/scala/slick/syntax/DBIOActionDbSyntax.scala
+++ db/src/main/scala/slick/syntax/DBIOActionDbSyntax.scala
@@ -1,6 +1,8 @@
 package slick.syntax
 
 import cats.effect.kernel.Async
+import cats.syntax.all.*
+import org.postgresql.util.{PSQLException, PSQLState}
 import slick.SlickDb
 import slick.dbio.{DBIOAction, Effect, NoStream}
 
@@ -12,4 +14,13 @@ trait DBIOActionRunSyntax {
 
 final class DBIOSyntaxOps[F[_], R, +S <: NoStream, -E <: Effect](val x: DBIOAction[R, S, E]) extends AnyVal {
   def run(implicit F: Async[F], slickDb: SlickDb): F[R] = slickDb.run(x)
+
+  def runRepeatedlyOrDefault(default: R, attempts: Int = 100)(implicit F: Async[F], slickDb: SlickDb): F[R] = {
+    def repeat[A](action: => F[A], attempts: Int): F[A] = action.handleErrorWith {
+      case ex: PSQLException if ex.getSQLState == PSQLState.SERIALIZATION_FAILURE.getState && attempts > 0 =>
+        repeat(action, attempts - 1)
+      case e                                                                                               => F.raiseError(e)
+    }
+    repeat(slickDb.run(x), attempts).handleError(_ => default)
+  }
 }
