94acbb5cd2e3f222522c58ee1d2af679c9f47e15
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-12-14 21:12:36 +0400

    Add comment

--- db/src/main/scala/slick/syntax/DBIOActionDbSyntax.scala
+++ db/src/main/scala/slick/syntax/DBIOActionDbSyntax.scala
@@ -15,6 +15,9 @@ trait DBIOActionRunSyntax {
 final class DBIOSyntaxOps[F[_], R, +S <: NoStream, -E <: Effect](val x: DBIOAction[R, S, E]) extends AnyVal {
   def run(implicit F: Async[F], slickDb: SlickDb): F[R] = slickDb
     .run(x)
+    // This error handling is to ensure that the action is run the second time if the first time it failed with
+    // UNIQUE_VIOLATION. This is to make concurrent inserts work for data that requires lookups, since Slick does not
+    // provide tools that raw SQL has to offer in such scenarios (forUpdate did not work, onConflict is not supported).
     .handleErrorWith {
       case ex: PSQLException if ex.getSQLState == PSQLState.UNIQUE_VIOLATION.getState => slickDb.run(x)
       case error                                                                      => F.raiseError(error)
