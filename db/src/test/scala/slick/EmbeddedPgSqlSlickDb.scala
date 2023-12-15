fd7d1091cec218ac1ea68aee5480741e55015353
stanislavlyalin <lyalinstas@gmail.com>
2023-12-15 16:56:43 +0300

    Fix typo

--- db/src/test/scala/slick/EmbeddedPgSqlSlickDb.scala
+++ db/src/test/scala/slick/EmbeddedPgSqlSlickDb.scala
@@ -3,9 +3,9 @@ package slick
 import cats.effect.kernel.Resource
 import cats.effect.{Async, Sync}
 import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
-import slick.jdbc.H2Profile
 import slick.jdbc.JdbcBackend.Database
-import slick.migration.api.H2Dialect
+import slick.jdbc.PostgresProfile
+import slick.migration.api.PostgresDialect
 
 object EmbeddedPgSqlSlickDb {
   def apply[F[_]: Async]: Resource[F, SlickDb] = {
@@ -17,6 +17,6 @@ object EmbeddedPgSqlSlickDb {
     )
     val dbResource =
       Resource.make(open)(db => Sync[F].delay(db.close())).map(x => Database.forDataSource(x.getPostgresDatabase, None))
-    SlickDb.resource(dbResource, H2Profile, new H2Dialect)
+    SlickDb.resource(dbResource, PostgresProfile, new PostgresDialect)
   }
 }
