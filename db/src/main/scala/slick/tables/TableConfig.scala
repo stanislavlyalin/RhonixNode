f9442b5fc46861de3c710fad55d8d685c036e38a
stanislavlyalin <lyalinstas@gmail.com>
2023-12-28 10:17:22 +0300

    Rename table according to accepted style

--- db/src/main/scala/slick/tables/TableConfig.scala
+++ db/src/main/scala/slick/tables/TableConfig.scala
@@ -3,7 +3,7 @@ package slick.tables
 import slick.jdbc.PostgresProfile.api.*
 import slick.lifted.ProvenShape
 
-class TableConfig(tag: Tag) extends Table[(String, String)](tag, "Config") {
+class TableConfig(tag: Tag) extends Table[(String, String)](tag, "config") {
   def name: Rep[String]  = column[String]("name", O.PrimaryKey)
   def value: Rep[String] = column[String]("value")
 
