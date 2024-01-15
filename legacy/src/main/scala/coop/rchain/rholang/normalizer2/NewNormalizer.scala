de214406d243f988fc784dfc520925276ef2f217
hilltracer <hilltracer@yandex.ru>
2024-01-15 09:52:46 +0300

    Refactoring NewNormalizer implementation

--- legacy/src/main/scala/coop/rchain/rholang/normalizer2/NewNormalizer.scala
+++ legacy/src/main/scala/coop/rchain/rholang/normalizer2/NewNormalizer.scala
@@ -14,26 +14,25 @@ object NewNormalizer {
   @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
   def normalizeNew[F[_]: Sync: NormalizerRec, T >: VarSort: BoundVarWriter: BoundVarReader](p: PNew): F[NewN] =
     Sync[F].defer {
-      // TODO: bindings within a single new shouldn't have overlapping names.
-      val newTaggedBindings = p.listnamedecl_.asScala.toVector.map {
-        case n: NameDeclSimpl => (None, n.var_, NameSort, n.line_num, n.col_num)
-        case n: NameDeclUrn   =>
-          (
-            Some(GroundNormalizeMatcher.stripUri(n.uriliteral_)),
-            n.var_,
-            NameSort,
-            n.line_num,
-            n.col_num,
-          )
+      val simpleBindings = p.listnamedecl_.asScala.toSeq.collect { case n: NameDeclSimpl =>
+        (n.var_, NameSort, SourcePosition(n.line_num, n.col_num))
       }
-      // This sorts the None's first, and the uris by lexicographical order.
-      // We do this here because the sorting affects the numbering of variables inside the body.
-      val sortBindings      = newTaggedBindings.sortBy(row => row._1)
-      val newBindings       = sortBindings.map(row => (row._2, row._3, SourcePosition(row._4, row._5)))
-      val uris              = sortBindings.flatMap(row => row._1)
+
+      val sortedUrnData = p.listnamedecl_.asScala.toSeq
+        .collect { case n: NameDeclUrn =>
+          (
+            GroundNormalizeMatcher.stripUri(n.uriliteral_),
+            (n.var_, NameSort, SourcePosition(n.line_num, n.col_num)),
+          )
+        }
+        .sortBy(_._1) // Sort by uris in lexicographical order
+
+      val (uris, urnBindings) = sortedUrnData.unzip
+
+      val newBindings = simpleBindings ++ urnBindings
 
       val initCount = BoundVarReader[T].boundVarCount
-      BoundVarWriter[T].putBoundVars(newBindings.toList)
+      BoundVarWriter[T].putBoundVars(newBindings)
       val bindCount = BoundVarReader[T].boundVarCount - initCount
 
       NormalizerRec[F]
