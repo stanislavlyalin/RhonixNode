53e08e36d2633a5b82ce8382878bc54daeccec0b
stanislavlyalin <lyalinstas@gmail.com>
2024-01-17 14:05:44 +0300

    Solution for `java.lang.IllegalArgumentException: requirement failed: count cannot be increased`: custom AsyncExecutor for EmbeddedPgSqlSlickDb

--- db/src/test/scala/slick/EmbeddedPgSqlSlickDb.scala
+++ db/src/test/scala/slick/EmbeddedPgSqlSlickDb.scala
@@ -6,17 +6,29 @@ import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
 import slick.jdbc.JdbcBackend.Database
 import slick.jdbc.PostgresProfile
 import slick.migration.api.PostgresDialect
+import slick.util.AsyncExecutor
 
 object EmbeddedPgSqlSlickDb {
   def apply[F[_]: Async]: Resource[F, SlickDb] = {
-    val open       = Sync[F].delay(
+    val open           = Sync[F].delay(
       EmbeddedPostgres
         .builder()
         .setOutputRedirector(ProcessBuilder.Redirect.to(new java.io.File("/tmp/embedPgSql.log")))
         .start(),
     )
-    val dbResource =
-      Resource.make(open)(db => Sync[F].delay(db.close())).map(x => Database.forDataSource(x.getPostgresDatabase, None))
+    val maxConnections = 100
+    val minThreads     = 100
+    val maxThreads     = 100
+    val dbResource     =
+      Resource
+        .make(open)(db => Sync[F].delay(db.close()))
+        .map(x =>
+          Database.forDataSource(
+            x.getPostgresDatabase,
+            maxConnections = Some(maxConnections),
+            executor = AsyncExecutor("EmbeddedPgExecutor", minThreads, maxThreads, 1000, maxConnections),
+          ),
+        )
 
     dbResource.evalMap(SlickDb(_, PostgresProfile, new PostgresDialect))
   }
