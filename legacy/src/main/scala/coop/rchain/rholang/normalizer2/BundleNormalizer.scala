ecf46cc8d3d6f287da7fd4a35560d79efc76cc10
hilltracer <hilltracer@yandex.ru>
2024-01-22 12:35:41 +0300

    Add bundle normalizer

---
+++ legacy/src/main/scala/coop/rchain/rholang/normalizer2/BundleNormalizer.scala
@@ -0,0 +1,43 @@
+package coop.rchain.rholang.normalizer2
+
+import cats.effect.Sync
+import cats.syntax.all.*
+import coop.rchain.rholang.interpreter.compiler.SourcePosition
+import coop.rchain.rholang.interpreter.errors.{InterpreterError, UnexpectedBundleContent}
+import coop.rchain.rholang.normalizer2.env.RestrictWriter
+import coop.rchain.rholang.syntax.normalizerEffectSyntax
+import io.rhonix.rholang.*
+import io.rhonix.rholang.ast.rholang.Absyn.*
+
+object BundleNormalizer {
+  def normalizeBundle[F[_]: Sync: NormalizerRec: RestrictWriter](p: PBundle): F[BundleN] = {
+    def connectivesExistOnTop(p: ParN): Boolean =
+      p match {
+        case _: ConnectiveN  => true
+        case pProc: ParProcN => pProc.ps.exists(connectivesExistOnTop)
+        case _               => false
+      }
+
+    def returnError: F[InterpreterError] = UnexpectedBundleContent(
+      s"Illegal top level connective in bundle at: ${SourcePosition(p.line_num, p.col_num)}.",
+    ).raiseError
+
+    for {
+      // Inside bundle target is prohibited to have free variables and wildcards.
+      target <- NormalizerRec[F].normalize(p.proc_).asBundle()
+      // Inside bundle target is prohibited to have connectives on top level.
+      _      <- returnError.whenA(connectivesExistOnTop(target))
+
+      outermostBundle = p.bundle_ match {
+                          case _: BundleReadWrite => BundleN(target, writeFlag = true, readFlag = true)
+                          case _: BundleRead      => BundleN(target, writeFlag = false, readFlag = true)
+                          case _: BundleWrite     => BundleN(target, writeFlag = true, readFlag = false)
+                          case _: BundleEquiv     => BundleN(target, writeFlag = false, readFlag = false)
+                        }
+
+    } yield target match {
+      case b: BundleN => outermostBundle.merge(b)
+      case _          => outermostBundle
+    }
+  }
+}
