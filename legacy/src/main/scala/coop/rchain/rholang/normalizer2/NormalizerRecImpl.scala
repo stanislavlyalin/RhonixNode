05bf82cf3d9986e218baccf77ee8cacb0a55aeb8
Tomislav Grospic <grospic@gmail.com>
2024-01-25 12:23:30 +0100

    Refactor NormalizerRec implementation

--- legacy/src/main/scala/coop/rchain/rholang/normalizer2/NormalizerRecImpl.scala
+++ legacy/src/main/scala/coop/rchain/rholang/normalizer2/NormalizerRecImpl.scala
@@ -1,45 +1,83 @@
 package coop.rchain.rholang.normalizer2
 
 import cats.effect.Sync
-import cats.implicits.{catsSyntaxApplicativeId, catsSyntaxOptionId, catsSyntaxTuple2Semigroupal, none, toFunctorOps}
+import cats.syntax.all.*
 import coop.rchain.rholang.interpreter.compiler.*
 import coop.rchain.rholang.interpreter.errors.*
 import coop.rchain.rholang.normalizer2.env.*
 import io.rhonix.rholang.*
 import io.rhonix.rholang.ast.rholang.Absyn.*
 
-final case class NormalizerRecImpl[F[
-  +_,
-]: Sync: BoundVarScope: FreeVarScope: NestingInfoWriter, T >: VarSort: BoundVarWriter: BoundVarReader: FreeVarWriter: FreeVarReader]()(
-  implicit nestingInfo: NestingInfoReader,
-) extends NormalizerRec[F] {
+final case class NormalizerRecImpl[
+  F[+_]: Sync: BoundVarScope: FreeVarScope: NestingInfoWriter,
+  T >: VarSort: BoundVarWriter: BoundVarReader: FreeVarWriter: FreeVarReader,
+]()(implicit nestingInfo: NestingInfoReader)
+    extends NormalizerRec[F] {
 
-  implicit val nRec: NormalizerRecImpl[F, T] = this
+  implicit val nRec: NormalizerRec[F] = this
 
-  override def normalize(proc: Proc): F[ParN] = Sync[F].defer {
+  override def normalize(proc: Proc): F[ParN] = NormalizerRecImpl.normalize[F, T](proc)
+
+  override def normalize(name: Name): F[ParN] = name match {
+    case nv: NameVar      => VarNormalizer.asBoundVar[F, T](nv.var_, SourcePosition(nv.line_num, nv.col_num), NameSort)
+    case nq: NameQuote    => NormalizerRec[F].normalize(nq.proc_)
+    case wc: NameWildcard => VarNormalizer.asWildcard[F](SourcePosition(wc.line_num, wc.col_num))
+  }
+
+  override def normalize(remainder: ProcRemainder): F[Option[VarN]] = remainder match {
+    case _: ProcRemainderEmpty => none.pure
+    case pr: ProcRemainderVar  => VarNormalizer.asRemainder[F, T](pr.procvar_).map(_.some)
+  }
+
+  override def normalize(remainder: NameRemainder): F[Option[VarN]] = remainder match {
+    case _: NameRemainderEmpty => none.pure
+    case nr: NameRemainderVar  => VarNormalizer.asRemainder[F, T](nr.procvar_).map(_.some)
+  }
+}
+
+object NormalizerRecImpl {
+
+  /** Normalizes parser AST types to core Rholang AST types
+   *
+   * @param proc input parser AST object
+   * @return core Rholang AST object [[ParN]]
+   */
+  def normalize[
+    F[+_]: Sync: NormalizerRec: BoundVarScope: FreeVarScope: NestingInfoWriter,
+    T >: VarSort: BoundVarWriter: BoundVarReader: FreeVarWriter: FreeVarReader,
+  ](proc: Proc)(implicit nestingInfo: NestingInfoReader): F[ParN] = {
+
     def unaryExp(subProc: Proc, constructor: ParN => ExprN): F[ParN] =
       NormalizerRec[F].normalize(subProc).map(constructor)
 
     def binaryExp(subProcLeft: Proc, subProcRight: Proc, constructor: (ParN, ParN) => ExprN): F[ParN] =
       (NormalizerRec[F].normalize(subProcLeft), NormalizerRec[F].normalize(subProcRight)).mapN(constructor)
 
-    // Create normalizedProc to not explicitly specify the type as F[ParN]
-    val normalizedProc = proc match {
-      case p: PNegation    => NegationNormalizer.normalizeNegation(p)
-      case p: PConjunction => ConjunctionNormalizer.normalizeConjunction(p)
-      case p: PDisjunction => DisjunctionNormalizer.normalizeDisjunction(p)
-      case p: PSimpleType  => Sync[F].delay(SimpleTypeNormalizer.normalizeSimpleType(p))
-      case p: PGround      => GroundNormalizer.normalizeGround(p)
-      case p: PCollect     => CollectNormalizer.normalizeCollect(p)
-      case p: PVar         => VarNormalizer.normalizeProcVar[F, T](p)
-      case p: PVarRef      => VarRefNormalizer.normalizeVarRef[F, T](p)
-      case _: PNil         => Sync[F].delay(NilN: ParN)
-      case p: PEval        => EvalNormalizer.normalizeEval(p)
-      case p: PMethod      => MethodNormalizer.normalizeMethod(p)
+    // Dispatch to normalizer methods depending on parser AST type
+    proc match {
+      /* Terminal expressions (0-arity constructors) */
+      /* =========================================== */
+      case _: PNil        => (NilN: ParN).pure
+      case p: PGround     => GroundNormalizer.normalizeGround[F](p)
+      case p: PVar        => VarNormalizer.normalizeProcVar[F, T](p)
+      case p: PVarRef     => VarRefNormalizer.normalizeVarRef[F, T](p)
+      case p: PSimpleType => Sync[F].delay(SimpleTypeNormalizer.normalizeSimpleType(p))
 
-      case p: PNot => unaryExp(p.proc_, ENotN.apply)
-      case p: PNeg => unaryExp(p.proc_, ENegN.apply)
+      /* Unary expressions (1-arity constructors) */
+      /* ======================================== */
+      case p: PEval     => EvalNormalizer.normalizeEval[F](p)
+      case p: PBundle   => BundleNormalizer.normalizeBundle[F](p)
+      case p: PNegation => NegationNormalizer.normalizeNegation[F](p)
+      case p: PExprs    => NormalizerRec[F].normalize(p.proc_)
+      case p: PNot      => unaryExp(p.proc_, ENotN.apply)
+      case p: PNeg      => unaryExp(p.proc_, ENegN.apply)
 
+      /* Binary expressions (2-arity constructors) */
+      /* ========================================= */
+      case p: PPar            => ParNormalizer.normalizePar[F](p)
+      case p: PMatches        => MatchesNormalizer.normalizeMatches[F](p)
+      case p: PConjunction    => ConjunctionNormalizer.normalizeConjunction[F](p)
+      case p: PDisjunction    => DisjunctionNormalizer.normalizeDisjunction[F](p)
       case p: PMult           => binaryExp(p.proc_1, p.proc_2, EMultN.apply)
       case p: PDiv            => binaryExp(p.proc_1, p.proc_2, EDivN.apply)
       case p: PMod            => binaryExp(p.proc_1, p.proc_2, EModN.apply)
@@ -59,42 +97,21 @@ final case class NormalizerRecImpl[F[
       case p: PShortAnd       => binaryExp(p.proc_1, p.proc_2, EShortAndN.apply)
       case p: PShortOr        => binaryExp(p.proc_1, p.proc_2, EShortOrN.apply)
 
-      case p: PMatches   => MatchesNormalizer.normalizeMatches(p)
-      case p: PExprs     => NormalizerRec[F].normalize(p.proc_)
-      case p: PSend      => SendNormalizer.normalizeSend(p)
-      case p: PSendSynch => SendSynchNormalizer.normalizeSendSynch(p)
-      case p: PContr     => ContrNormalizer.normalizeContr(p)
+      /* N-ary parameter expressions (N-arity constructors) */
+      /* ================================================== */
+      case p: PCollect   => CollectNormalizer.normalizeCollect[F](p)
+      case p: PSend      => SendNormalizer.normalizeSend[F](p)
+      case p: PSendSynch => SendSynchNormalizer.normalizeSendSynch[F](p)
+      case p: PContr     => ContrNormalizer.normalizeContr[F, T](p)
       case p: PInput     => InputNormalizer.normalizeInput(p)
-      case p: PPar       => ParNormalizer.normalizePar(p)
       case p: PNew       => NewNormalizer.normalizeNew[F, T](p)
-      case p: PBundle    => BundleNormalizer.normalizeBundle(p)
-      case p: PLet       => LetNormalizer.normalizeLet(p)
-      case p: PMatch     => MatchNormalizer.normalizeMatch(p)
-      case p: PIf        => IfNormalizer.normalizeIf(p)
-      case p: PIfElse    => IfElseNormalizer.normalizeIfElse(p)
+      case p: PLet       => LetNormalizer.normalizeLet[F, T](p)
+      case p: PMatch     => MatchNormalizer.normalizeMatch[F, T](p)
+      case p: PIf        => IfNormalizer.normalizeIf[F](p)
+      case p: PIfElse    => IfElseNormalizer.normalizeIfElse[F](p)
+      case p: PMethod    => MethodNormalizer.normalizeMethod[F](p)
 
-      case _ => Sync[F].raiseError(UnrecognizedNormalizerError("Compilation of construct not yet supported."))
-    }
-    normalizedProc
-  }
-
-  override def normalize(name: Name): F[ParN] = Sync[F].defer {
-    name match {
-      case nv: NameVar      => VarNormalizer.asBoundVar[F, T](nv.var_, SourcePosition(nv.line_num, nv.col_num), NameSort)
-      case nq: NameQuote    => NormalizerRec[F].normalize(nq.proc_)
-      case wc: NameWildcard => VarNormalizer.asWildcard[F](SourcePosition(wc.line_num, wc.col_num))
+      case _ => UnrecognizedNormalizerError("Compilation of construct not yet supported.").raiseError
     }
   }
-
-  override def normalize(remainder: ProcRemainder): F[Option[VarN]] =
-    remainder match {
-      case _: ProcRemainderEmpty => none.pure
-      case pr: ProcRemainderVar  => VarNormalizer.asRemainder[F, T](pr.procvar_).map(_.some)
-    }
-
-  override def normalize(remainder: NameRemainder): F[Option[VarN]] =
-    remainder match {
-      case _: NameRemainderEmpty => none.pure
-      case nr: NameRemainderVar  => VarNormalizer.asRemainder[F, T](nr.procvar_).map(_.some)
-    }
 }
