03c184450a5ee33aa6956a55b32e7f817a64ce34
stanislavlyalin <lyalinstas@gmail.com>
2024-02-02 10:31:39 +0300

    Move existing gRPC test to work with new GrpcClient & GrpcServer

--- node/src/test/scala/node/GrpcDslSpec.scala
+++ node/src/test/scala/node/GrpcDslSpec.scala
@@ -1,13 +1,14 @@
 package node
 
+import cats.effect.unsafe.implicits.global
+import cats.effect.{IO, Sync}
 import com.google.protobuf.{CodedInputStream, CodedOutputStream}
 import io.grpc.*
-import io.grpc.netty.{NettyChannelBuilder, NettyServerBuilder}
+import node.comm.{GrpcClient, GrpcServer, Logger}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import java.io.{InputStream, PipedInputStream, PipedOutputStream}
-import java.time.LocalDateTime
 
 class GrpcDslSpec extends AnyFlatSpec with Matchers {
 
@@ -53,108 +54,30 @@ class GrpcDslSpec extends AnyFlatSpec with Matchers {
     .setResponseMarshaller(myObjMarshal)
     .build()
 
-  /// An example how to execute client requests to the server (host) and respond
-  def sendClientRequest(message: String): ManagedChannel = {
-    // Creates channel object with the pointer to the server (host)
-    val channel = NettyChannelBuilder
-      .forAddress("localhost", 4321)
-      .usePlaintext()
-      .build
+  "grpc server & client" should "be defined with low level API directly" in {
+    val serverHost = "localhost"
+    val serverPort = 4321
+    val srcMessage = MyObj("Hello from client!", 42)
 
-    // Creates new object that represents base gRPC API (to send/receive messages)
-    val call = channel.newCall[MyObj, MyObj](method, CallOptions.DEFAULT)
+    implicit val m: MethodDescriptor[MyObj, MyObj] = method
+    implicit val l: Logger                         = Logger.console
 
-    // Listener/callback to handle server responses
-    val callListener = new ClientCall.Listener[MyObj] {
-      override def onHeaders(headers: Metadata): Unit                =
-        info(s"CLIENT_ON_HEADERS: $headers")
-      override def onMessage(message: MyObj): Unit                   =
-        info(s"CLIENT_ON_MESSAGE: $message")
-      override def onClose(status: Status, trailers: Metadata): Unit =
-        info(s"CLIENT_ON_CLOSE: ${status}, $trailers")
-    }
-
-    // Must be called first, starts the call (headers are sent here)
-    call.start(callListener, /* headers = */ new Metadata())
-    // Sends real network request (serialization to InputStream via Marshaller is done here)
-    // Can be called multiple times which is "streaming" mode
-    call.sendMessage(MyObj(message, 42))
-    info(s"CLIENT_SENT_MSG: $message")
-    // Number of messages to read next from the response (default is no read at all)
-    // CHECK: Where is the buffer, client or server side
-    call.request(1)
-    // Clients marks no more messages with half-close
-    call.halfClose()
-    info(s"CLIENT_HALF_CLOSED")
-
-    channel
-  }
-
-  /// An example how to start the server and listed for client requests and respond
-  def startServer(port: Int): Server = {
-    val serverCallHandler: ServerCallHandler[MyObj, MyObj] =
-      new ServerCallHandler[MyObj, MyObj] {
-        override def startCall(
-          call: ServerCall[MyObj, MyObj],
-          headers: Metadata,
-        ): ServerCall.Listener[MyObj] = {
-          info(s"SERVER_START_CALL: ${call.getMethodDescriptor}")
-          info(s"SERVER_START_CALL: $headers")
-
-          // Number of messages to read next from the response (default is no read at all)
-          call.request(1)
-
-          // Server listener of clients requests
-          new ServerCall.Listener[MyObj] {
-            // Handle client request message and optionally respond
-            override def onMessage(message: MyObj): Unit = {
-              info(s"SERVER_ON_MESSAGE: $message")
-              // Sends headers
-              call.sendHeaders(headers)
-              // Sends the message
-              call.sendMessage(MyObj(s"Server responds to: $message", 42))
-              info(s"SERVER_SENT_RESPOND_MSG")
-              // Close must be called, but only once to prevent exception
-              call.close(Status.OK, headers)
-
-              // An example of server error
-              // call.close(Status.UNKNOWN.withDescription("Custom server error"), new Metadata())
-            }
-
-            override def onHalfClose(): Unit = info(s"SERVER_ON_HALF_CLOSE")
-            override def onCancel(): Unit    = info(s"SERVER_ON_CANCEL")
-            override def onComplete(): Unit  = info(s"SERVER_ON_COMPLETE")
-          }
-        }
-      }
-
-    // Service (API) description, mapping of methods specification with handlers
     val serviceDef = ServerServiceDefinition
       .builder("coop.rchain.Service")
-      .addMethod(method, serverCallHandler)
+      .addMethod(method, GrpcServer.makeCallHandler { req: MyObj => MyObj(s"Server responds to: $req", 42) })
       .build()
 
-    // Bind listener to the network adapter accepting API schema (service definition)
-    NettyServerBuilder
-      .forPort(port)
-      .addService(serviceDef)
-      .build
-      .start()
-  }
+    GrpcServer
+      .apply[IO](serverPort, serviceDef)
+      .use { _ =>
+        GrpcClient.apply[IO](serverHost, serverPort).use { client =>
+          for {
+            resp <- client.send(srcMessage)
+            _    <- Sync[IO].delay(Thread.sleep(250))
+          } yield resp shouldBe MyObj(s"Server responds to: $srcMessage", 42)
 
-  def info(msg: Any): Unit = {
-    val date = LocalDateTime.now()
-    val time = date.toLocalTime
-    println(s"[$time] $msg")
-  }
-
-  "grpc server & client" should "be defined with low level API directly" in {
-    val server = startServer(4321)
-
-    val channel = sendClientRequest("Hello from client!")
-
-    Thread.sleep(250)
-    channel.shutdown()
-    server.shutdown()
+        }
+      }
+      .unsafeRunSync()
   }
 }
