42eb753b86a255ec4ca12707a1d587439ce7533b
stanislavlyalin <lyalinstas@gmail.com>
2024-02-02 10:30:11 +0300

    Add GrpcServer and GrpcClient implementation with Logger

---
+++ node/src/main/scala/node/comm/GrpcClient.scala
@@ -0,0 +1,62 @@
+package node.comm
+
+import cats.effect.{Async, Resource, Sync}
+import cats.syntax.all.*
+import io.grpc.*
+import io.grpc.netty.NettyChannelBuilder
+
+import scala.concurrent.Promise
+
+class GrpcClient[F[_]: Async] private (private val channel: ManagedChannel) {
+  def send[Req, Resp](msg: Req)(implicit method: MethodDescriptor[Req, Resp], logger: Logger): F[Resp] = for {
+    response <- Async[F].fromFuture {
+                  Sync[F].delay {
+                    val call = channel.newCall[Req, Resp](method, CallOptions.DEFAULT)
+
+                    val promise = Promise[Resp]()
+
+                    val callListener = new ClientCall.Listener[Resp] {
+                      override def onHeaders(headers: Metadata): Unit =
+                        logger.info(s"CLIENT_ON_HEADERS: $headers")
+
+                      override def onMessage(message: Resp): Unit = {
+                        logger.info(s"CLIENT_ON_MESSAGE: $message")
+                        val _ = promise.success(message)
+                        super.onMessage(message)
+                      }
+
+                      override def onClose(status: Status, trailers: Metadata): Unit =
+                        logger.info(s"CLIENT_ON_CLOSE: $status, $trailers")
+                    }
+
+                    call.start(callListener, new Metadata())
+
+                    call.sendMessage(msg)
+                    logger.info(s"CLIENT_SENT_MSG: $msg")
+
+                    call.request(1)
+
+                    call.halfClose()
+                    logger.info(s"CLIENT_HALF_CLOSED")
+
+                    promise.future
+                  }
+                }
+  } yield response
+
+  def shutdown: ManagedChannel = channel.shutdown()
+}
+
+object GrpcClient {
+  def apply[F[_]: Async](serverHost: String, serverPort: Int): Resource[F, GrpcClient[F]] =
+    Resource.make {
+      Sync[F].delay {
+        val channel = NettyChannelBuilder
+          .forAddress(serverHost, serverPort)
+          .usePlaintext()
+          .build
+
+        new GrpcClient[F](channel)
+      }
+    }(client => Sync[F].delay(client.shutdown).void)
+}
