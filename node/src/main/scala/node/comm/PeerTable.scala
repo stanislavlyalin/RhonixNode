6306683746ff2de4113df25d7be84e9adb4cf3d3
nutzipper <1746367+nzpr@users.noreply.github.com>
2024-02-20 17:42:05 +0400

    Mk peer table accept slickApi

--- node/src/main/scala/node/comm/PeerTable.scala
+++ node/src/main/scala/node/comm/PeerTable.scala
@@ -5,7 +5,7 @@ import cats.effect.{Async, Sync}
 import cats.syntax.all.*
 import node.comm.PeerTable.ST
 import sdk.comm.Peer
-import slick.SlickDb
+import slick.api.SlickApi
 import slick.syntax.all.DBIOActionRunSyntax
 
 final class PeerTable[F[_]: Sync, PId, P] private (
@@ -31,27 +31,29 @@ final class PeerTable[F[_]: Sync, PId, P] private (
 object PeerTable {
   final case class ST[PId, P](peers: Map[PId, P])
 
-  def apply[F[_]: Async](cfg: Config)(implicit db: SlickDb): F[PeerTable[F, String, Peer]] = for {
-    api     <- slick.api.SlickApi[F].apply(db)
-    dbPeers <- api.actions.peers.run
-    state   <- if (dbPeers.isEmpty) {
-                 // At the first start, when there are no peers in the DB,
-                 // read them from the configuration file and save in the DB
-                 val peerTable = PeerTable.ST[String, Peer](cfg.peers.map { p =>
-                   p.url -> Peer(p.url, p.isSelf, p.isValidator)
-                 }.toMap)
-                 cfg.peers
-                   .traverse(p => api.actions.peerInsertIfNot(p.url, p.isSelf, p.isValidator).run)
-                   .as(peerTable)
-               } else {
-                 // During the next launches use peers from the DB
-                 PeerTable.ST[String, Peer](dbPeers.map(p => p.url -> p).toMap).pure
-               }
-    stCell  <- AtomicCell[F].of(state)
-  } yield new PeerTable(
-    stCell,
-    () => api.actions.peers.run,
-    (peer: Peer) => api.actions.peerInsertIfNot(peer.url, peer.isSelf, peer.isValidator).run.void,
-    (peer: Peer) => api.actions.removePeer(peer.url).run.void,
-  )
+  def apply[F[_]: Async](cfg: Config)(implicit api: SlickApi[F]): F[PeerTable[F, String, Peer]] = {
+    import api.slickDb
+    for {
+      dbPeers <- api.peers
+      state   <- if (dbPeers.isEmpty) {
+                   // At the first start, when there are no peers in the DB,
+                   // read them from the configuration file and save in the DB
+                   val peerTable = PeerTable.ST[String, Peer](cfg.peers.map { p =>
+                     p.host -> Peer(p.host, p.port, p.isSelf, p.isValidator)
+                   }.toMap)
+                   cfg.peers
+                     .traverse(p => api.actions.peerInsertIfNot(p.host, p.port, p.isSelf, p.isValidator).run)
+                     .as(peerTable)
+                 } else {
+                   // During the next launches use peers from the DB
+                   PeerTable.ST[String, Peer](dbPeers.map(p => p.host -> p).toMap).pure
+                 }
+      stCell  <- AtomicCell[F].of(state)
+    } yield new PeerTable(
+      stCell,
+      () => api.peers,
+      (peer: Peer) => api.actions.peerInsertIfNot(peer.host, peer.port, peer.isSelf, peer.isValidator).run.void,
+      (peer: Peer) => api.actions.removePeer(peer.host).run.void,
+    )
+  }
 }
