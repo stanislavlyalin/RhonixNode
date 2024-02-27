194face0d433970e0d9c6e6826eff7dfe73e27d3
nutzipper <1746367+nzpr@users.noreply.github.com>
2024-02-27 17:04:42 +0400

    Remove obsolete TODO

--- dproc/src/main/scala/dproc/data/Block.scala
+++ dproc/src/main/scala/dproc/data/Block.scala
@@ -24,15 +24,15 @@ final case class Block[M, S, T](
   sender: S,
   minGenJs: Set[M],
   offences: Set[M],
-  txs: List[T],              // TODO: change to Set[T]
+  txs: List[T], // TODO: change to Set[T]
   finalFringe: Set[M],
   finalized: Option[ConflictResolution[T]],
   merge: Set[T],
   bonds: Bonds[S],
   lazTol: Int,
   expThresh: Int,
-  finalStateHash: ByteArray, // TODO: change type to ByteArray
-  postStateHash: ByteArray,  // TODO: change type to ByteArray
+  finalStateHash: ByteArray,
+  postStateHash: ByteArray,
 )
 
 object Block {
