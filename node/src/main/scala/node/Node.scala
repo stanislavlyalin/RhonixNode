bd755b0f034adf8a2eece6b0cf3179c1cdaeb811
nutzipper <1746367+nzpr@users.noreply.github.com>
2024-03-12 13:15:32 +0400

    Wait for bootstrap when starting

--- node/src/main/scala/node/Node.scala
+++ node/src/main/scala/node/Node.scala
@@ -1,61 +1,113 @@
 package node
 
 import cats.Parallel
-import cats.effect.std.Console
-import cats.effect.{Async, Sync}
+import cats.effect.Async
 import cats.syntax.all.*
 import fs2.Stream
-import node.comm.CommImpl
+import node.rpc.syntax.all.grpcClientSyntax
+import node.rpc.{GrpcChannelsManager, GrpcClient}
 import sdk.crypto.ECDSA
-import sdk.data.{BalancesDeploy, BalancesState}
+import sdk.data.BalancesState
 import sdk.diag.Metrics
+import sdk.log.Logger.*
 import sdk.primitive.ByteArray
 import secp256k1.Secp256k1
 import weaver.data.FinalData
 
-import scala.concurrent.duration.DurationInt
+import java.net.InetSocketAddress
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 object Node {
 
-  def apply[F[_]: Async: Parallel: Console](
+  def createGenesis[F[_]: Async: Parallel](
+    id: ByteArray,
+    genesisPoS: FinalData[ByteArray],
+    genesisBalances: BalancesState,
+    setup: Setup[F],
+  ): Stream[F, Unit] = {
+    implicit val m: Metrics[F] = Metrics.unit
+    Stream.eval(for {
+      _ <- logInfoF(s"Creating genesis block.")
+      g <- Genesis.mkGenesisBlock[F](id, genesisPoS, genesisBalances, setup.balancesShard)
+      _ <- logDebugF(s"Genesis block created with hash ${g.id}.")
+      _ <- DbApiImpl(setup.database).saveBlock(g)
+      _ <- logDebugF(s"Genesis block saved.")
+      _ <- setup.dProc.acceptMsg(g.id)
+      _ <- logInfoF(s"Genesis complete.")
+    } yield ())
+  }
+
+  private def waitTillResolved[F[_]: Async](
+    isa: InetSocketAddress,
+    time: FiniteDuration = 2.second,
+  ): F[InetSocketAddress] =
+    isa.tailRecM { isa =>
+      if (isa.isUnresolved)
+        logInfoF(s"Bootstrap host ${isa.getHostName} is unavailable. Retry in ${time.toSeconds} seconds.") *>
+          Async[F].sleep(time) *>
+          Async[F].delay(new InetSocketAddress(isa.getHostName, isa.getPort).asLeft[InetSocketAddress])
+      else
+        Async[F].delay(isa.asRight[InetSocketAddress])
+    }
+
+  private def waitTillLatestMessageIsAvailableAndGet[F[_]: Async: GrpcChannelsManager](
+    bootstrap: InetSocketAddress,
+    time: FiniteDuration = 2.second,
+  ): F[Seq[ByteArray]] =
+    ().tailRecM { _ =>
+      // TODO make API methods return errors?
+      GrpcClient[F]
+        .getLatestBlocks(bootstrap)
+        .flatTap(x => new Exception(s"Empty latest messages.").raiseError.whenA(x.isEmpty))
+        .attempt
+        .flatMap {
+          case Right(lms) =>
+            logInfoF(
+              s"Query for latest messages from $bootstrap succeed: $lms",
+            ) *> Async[F].delay(lms.asRight[Unit])
+          case Left(e)    =>
+            logInfoF(
+              s"Query for latest messages from $bootstrap failed: ${e.getLocalizedMessage}. Retry in $time seconds.",
+            ) *> Async[F].sleep(time).as(().asLeft[Seq[ByteArray]])
+        }
+    }
+
+  private def bootstrap[F[_]: Async: GrpcChannelsManager](
+    bootstrap: InetSocketAddress,
+    blockResolver: BlockResolver[F],
+  ): Stream[F, Unit] = Stream.eval {
+    for {
+      resolved <- waitTillResolved(bootstrap)
+      tips     <- waitTillLatestMessageIsAvailableAndGet(resolved)
+      _        <- tips.traverse(blockResolver.in(_, resolved))
+    } yield ()
+  }
+
+  def apply[F[_]: Async: Parallel](
     id: ByteArray,
     isBootstrap: Boolean,
     setup: Setup[F],
     genesisPoS: FinalData[ByteArray],
     genesisBalances: BalancesState,
+    bootstrapOpt: Option[InetSocketAddress] = None,
   ): fs2.Stream[F, Unit] = {
-    val printDiag: fs2.Stream[F, List[NodeSnapshot[ByteArray, ByteArray, BalancesDeploy]]] = NodeSnapshot
+    val printDiag: Stream[F, Unit] = NodeSnapshot
       .stream(setup.stateManager, setup.dProc.finStream)
-      .map(List(_))
-      .metered(100.milli)
-      .printlns
+      .metered(3000.milli)
+      .evalMap(x => logDebugF(x.show))
 
-    val mkGenesis: Stream[F, Unit] = {
-      implicit val m: Metrics[F] = Metrics.unit
-      Stream.eval(
-        Genesis
-          .mkGenesisBlock[F](id, genesisPoS, genesisBalances, setup.balancesShard)
-          .flatMap { genesisM =>
-            DbApiImpl(setup.database).saveBlock(genesisM) *>
-              setup.ports.sendToInput(CommImpl.BlockHash(genesisM.id)) *>
-              Sync[F].delay(sdk.log.Logger.console.info(s"Genesis block created with hash ${genesisM.id}"))
-          },
-      )
-    }
+    val init =
+      if (isBootstrap) createGenesis[F](id, genesisPoS, genesisBalances, setup)
+      else {
+        implicit val gcm: GrpcChannelsManager[F] = setup.grpcChannelsManager
+        bootstrap(bootstrapOpt.get, setup.blockResolver)
+      }
 
-    val genesisF =
-      if (isBootstrap)
-        Stream.eval(Console[F].println(s"Minting genesis")) ++
-          mkGenesis ++
-          Stream.eval(Console[F].println(s"Genesis complete"))
-      else Stream.eval(Console[F].println(s"Bootstrapping"))
+    val run: Stream[F, Unit] = setup.nodeStream
 
-    val run: Stream[F, Unit] = setup.dProc.dProcStream
-
-    (genesisF ++ run) concurrently printDiag
+    (init ++ printDiag) concurrently run
   }
 
   val SupportedECDSA: Map[String, ECDSA] = Map("secp256k1" -> Secp256k1.apply)
   val BalancesShardName: String          = "balances"
-
 }
