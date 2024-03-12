64b5b6ab4d2bab777237950d8837601a288b91a0
nutzipper <1746367+nzpr@users.noreply.github.com>
2024-03-12 13:00:41 +0400

    Resolve future in case of grpc failure. CALL CAN STUCK OTHERWISE!

--- node/src/main/scala/node/rpc/GrpcClient.scala
+++ node/src/main/scala/node/rpc/GrpcClient.scala
@@ -2,7 +2,7 @@ package node.rpc
 
 import cats.effect.{Async, Sync}
 import io.grpc.*
-import sdk.log.Logger
+import sdk.log.Logger.*
 
 import scala.concurrent.Promise
 
@@ -19,6 +19,8 @@ trait GrpcClient[F[_]] {
 }
 
 object GrpcClient {
+  private def callFailure(errMsg: String, cause: Throwable): Throwable = new RuntimeException(errMsg).initCause(cause)
+
   def apply[F[_]: Async]: GrpcClient[F] =
     new GrpcClient[F] {
       override def call[Req, Resp](method: MethodDescriptor[Req, Resp], msg: Req, channel: Channel): F[Resp] = {
@@ -30,27 +32,41 @@ object GrpcClient {
 
           val callListener = new ClientCall.Listener[Resp] {
             override def onHeaders(headers: Metadata): Unit =
-              Logger.console.info(s"CLIENT_ON_HEADERS: $headers")
+              logDebug(s"CLIENT_ON_HEADERS: $headers")
+
+            override def onReady(): Unit = {
+              logDebug("CLIENT_ON_READY")
+              super.onReady()
+            }
 
             override def onMessage(message: Resp): Unit = {
-              Logger.console.info(s"CLIENT_ON_MESSAGE: $message")
+              logDebug(s"CLIENT_ON_MESSAGE: $message")
               val _ = promise.success(message)
               super.onMessage(message)
             }
 
-            override def onClose(status: Status, trailers: Metadata): Unit =
-              Logger.console.info(s"CLIENT_ON_CLOSE: $status, $trailers")
+            override def onClose(status: Status, trailers: Metadata): Unit = {
+              val _ =
+                if (status != Status.OK)
+                  promise.failure(
+                    callFailure(
+                      s"Failed to send message $msg through channel $channel",
+                      status.asRuntimeException(),
+                    ),
+                  )
+              logDebug(s"CLIENT_ON_CLOSE: $status, $trailers")
+            }
           }
 
           call.start(callListener, new Metadata())
 
           call.sendMessage(msg)
-          Logger.console.info(s"CLIENT_SENT_MSG: $msg")
+          logDebug(s"CLIENT_SENT_MSG: $msg")
 
           call.request(1)
 
           call.halfClose()
-          Logger.console.info(s"CLIENT_HALF_CLOSED")
+          logDebug(s"CLIENT_HALF_CLOSED")
 
           promise.future
         }
